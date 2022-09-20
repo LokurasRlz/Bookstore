a2faf9953af046b1fe5de40cfc5861a901633e93
Matias Aguirre <aguirremati09@gmail.com>
2022-09-20 19:29:14 -0300

    Updated Categories Status

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,17 +1,18 @@
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { checkStatusBook } from '../redux/categories/categories';
 
 const Categories = () => {
   const dispatch = useDispatch();
-
-  const checkStatus = (e) => {
-    dispatch(checkStatusBook(e.target.value));
+  const handleStatus = () => {
+    dispatch(checkStatusBook());
   };
-
+  const status = useSelector((state) => state.categoriesReducer);
   return (
-    <section className="categories-section">
-      <button type="button" className="btn status" onClick={checkStatus}>CHECK STATUS</button>
-    </section>
+    <>
+      <button className="Check-status" onClick={handleStatus} type="button">CHECK STATUS</button>
+      <h2>{status}</h2>
+    </>
   );
 };
 export default Categories;
